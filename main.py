673bda5e2b3c1d7d850a707ef45d71feef8dfede
Tero <tero@collin.com>
2021-09-05 11:16:52 -0700

    sqlite address fix

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ Bootstrap(app)
 ##CONNECT TO DB
 # Update the app config to use "DATABASE_URL" environment variable if provided,
 # but if it's None (e.g. when running locally) then we can provide sqlite:///blog.db as the alternative.
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite://blog.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
